2b18bd5aa55673123354e3090c0fa8dfb30a3e5a
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-04-26 11:15:42 +0300

    Show file-list in specific folder

--- MxFileScanner-Console/main.cpp
+++ MxFileScanner-Console/main.cpp
@@ -1,8 +1,56 @@
 #include <QtCore/QCoreApplication>
+#include <QDir>
+#include <clocale>
+
+int filesCount = 0;
+int filesSize = 0;
+int foldersCount = 0;
+
+void fileList(QDir& root)
+{
+    foreach(QFileInfo fi, root.entryInfoList(QDir::AllEntries | QDir::NoDotAndDotDot, QDir::Name))
+    {
+        if (fi.isFile())
+        {
+            filesCount++;
+            filesSize += fi.size();
+
+            qInfo() << "[" << filesCount << "]" << "File: " << fi.filePath() << "\t" << fi.size();
+        }
+
+        if (fi.isDir())
+        {
+            foldersCount++;
+            // qInfo() << "Dir: " << fi.filePath();
+            QDir childDir(fi.filePath());
+
+            fileList(childDir);
+        }
+    }
+}
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    setlocale(LC_ALL, "Russian");
+
+    QString path = "D:/YandexDisk/Temp";
+    //QString path = QCoreApplication::applicationDirPath();
+
+    // Header information
+    qInfo() << "Start folder: " + path;
+    qInfo() << "----------------";
+
+    QDir dir(path);
+    fileList(dir);
+
+    // Footer information
+    qInfo() << "----------------";
+    qInfo() << "Files: " + QString::number(filesCount);
+    qInfo() << "Files size: " + QString::number(filesSize);
+    qInfo() << "Folders: " + QString::number(foldersCount);
 
     return a.exec();
 }
