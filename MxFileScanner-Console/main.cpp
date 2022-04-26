f1c057a73042248adb66a16c9d7454f5dae53472
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-04-26 15:49:23 +0300

    Create function to copy files all from fixed path

--- MxFileScanner-Console/main.cpp
+++ MxFileScanner-Console/main.cpp
@@ -1,10 +1,21 @@
 #include <QtCore/QCoreApplication>
-#include <QDir>
+#include <QDebug>
 #include <clocale>
+#include <QList>
+#include <QDir>
+#include <QQueue>
 
 int filesCount = 0;
-int filesSize = 0;
+qint64 filesSize = 0;
 int foldersCount = 0;
+
+//QString pathSource = "D:/YandexDisk/Temp";
+QString pathSource = "D:/YandexDisk/Work";
+QString pathDestination = "C:/Temp";
+
+QQueue<QString> filesQueue;
+QList<QString> dirs;
+
 
 void fileList(QDir& root)
 {
@@ -15,13 +26,16 @@ void fileList(QDir& root)
             filesCount++;
             filesSize += fi.size();
 
-            qInfo() << "[" << filesCount << "]" << "File: " << fi.filePath() << "\t" << fi.size();
+            qInfo() << "[" << filesCount << "]" << "File: " << fi.filePath();
+            filesQueue.enqueue(fi.filePath());
         }
 
         if (fi.isDir())
         {
             foldersCount++;
+            dirs.append(fi.filePath());
             // qInfo() << "Dir: " << fi.filePath();
+
             QDir childDir(fi.filePath());
 
             fileList(childDir);
@@ -36,21 +50,51 @@ int main(int argc, char *argv[])
 
     setlocale(LC_ALL, "Russian");
 
-    QString path = "D:/YandexDisk/Temp";
     //QString path = QCoreApplication::applicationDirPath();
 
     // Header information
-    qInfo() << "Start folder: " + path;
+    qInfo() << "Start folder: " + pathSource;
     qInfo() << "----------------";
 
-    QDir dir(path);
+    // Add files to queue
+    QDir dir(pathSource);
     fileList(dir);
+
+    qInfo() << "Queue: " + QString::number(filesQueue.count());
+
+    // Create destination folders
+    qInfo() << "Create destination folders";
+    for (qsizetype i = 0; i < dirs.size(); ++i)
+    {
+        QDir dir(dirs.at(i));
+        QString newDir = dir.absolutePath();
+        newDir.replace(pathSource, pathDestination);
+
+        dir.mkpath(newDir);
+        // dir.mkdir(newDir);
+        qInfo() << "Create folder: " << newDir;
+    }
+
+    // Copy selected files
+    while (!filesQueue.isEmpty())
+    {
+        QString fileSource = filesQueue.head();
+        QString fileDestination = fileSource;
+        fileDestination.replace(pathSource, pathDestination);
+        qInfo() << "New path: " << fileDestination;
+
+        QFile::copy(fileSource, fileDestination);
+
+        filesQueue.dequeue();
+    }
+
 
     // Footer information
     qInfo() << "----------------";
     qInfo() << "Files: " + QString::number(filesCount);
     qInfo() << "Files size: " + QString::number(filesSize);
-    qInfo() << "Folders: " + QString::number(foldersCount);
+    qInfo() << "Folders: " + QString::number(foldersCount);   
+
 
     return a.exec();
 }
